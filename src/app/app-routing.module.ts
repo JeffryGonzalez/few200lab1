eb44ed91cb5226e6de71aca1bbaf1c236ec4f687
Jeffry Gonzalez <jeff@hypertheory.com>
2020-03-24 07:08:07 -0400

    done with part 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TipCalculatorComponent } from './components/tip-calculator/tip-calculator.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'tip-calculator',
+    component: TipCalculatorComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavComponent } from './components/nav/nav.component';
+import { TipCalculatorComponent } from './components/tip-calculator/tip-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    NavComponent,
+    TipCalculatorComponent
   ],
   imports: [
     BrowserModule,
